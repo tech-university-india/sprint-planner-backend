0dec4ee103959b3cf5c5569d4b930a8da1b57580
Anshuman Ray <rayanshuman98@gmail.com>
2023-02-10 20:13:09 +0530

    feat: test services and controller

---
+++ test/controllers/project.test.js
@@ -0,0 +1,108 @@
+const projectServices = require('../../src/services/projects');
+const projectController = require('../../src/controllers/projects');
+
+describe('Project Controller', () => {
+  describe('createProject', () => {
+    it('should return 500 if projectServices.createProject throws an error', async () => {
+      const mockReq = {
+        body: {
+          title: 'Test Project',
+          sprintDuration: 2,
+          stories: [],
+          developers: [],
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      jest
+        .spyOn(projectServices, 'createProject')
+        .mockRejectedValue(new Error('Test Error'));
+      await projectController.createProject(mockReq, mockRes);
+      expect(mockRes.status).toBeCalledWith(500);
+      expect(mockRes.json).toBeCalledWith({ message: 'Test Error' });
+    });
+
+    it('should return 201 if projectServices.createProject returns a project', async () => {
+      const mockReq = {
+        body: {
+          title: 'Test Project',
+          sprintDuration: 2,
+          stories: [],
+          developers: [],
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const mockResult = { id: 1, title: 'Test Project' };
+      jest
+        .spyOn(projectServices, 'createProject')
+        .mockResolvedValue(mockResult);
+      await projectController.createProject(mockReq, mockRes);
+      expect(mockRes.status).toBeCalledWith(201);
+      expect(mockRes.json).toBeCalledWith({
+        message: 'Project created successfully',
+        data: mockResult,
+      });
+    });
+  });
+
+  describe('getProject', () => {
+    it('should return 500 if projectServices.getProject throws an error', async () => {
+      const mockReq = {
+        params: {
+          id: 1,
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      jest
+        .spyOn(projectServices, 'getProject')
+        .mockRejectedValue(new Error('Test Error'));
+      await projectController.getProject(mockReq, mockRes);
+      expect(mockRes.status).toBeCalledWith(500);
+      expect(mockRes.json).toBeCalledWith({ message: 'Test Error' });
+    });
+
+    it('should return 404 if projectServices.getProject returns null', async () => {
+      const mockReq = {
+        params: {
+          id: 1,
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      jest.spyOn(projectServices, 'getProject').mockResolvedValue(null);
+      await projectController.getProject(mockReq, mockRes);
+      expect(mockRes.status).toBeCalledWith(404);
+      expect(mockRes.json).toBeCalledWith({ message: 'Project not found' });
+    });
+
+    it('should return 200 if projectServices.getProject returns a project', async () => {
+      const mockReq = {
+        params: {
+          id: 1,
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const mockResult = { id: 1, title: 'Test Project' };
+      jest.spyOn(projectServices, 'getProject').mockResolvedValue(mockResult);
+      await projectController.getProject(mockReq, mockRes);
+      expect(mockRes.status).toBeCalledWith(200);
+      expect(mockRes.json).toBeCalledWith({
+        message: 'Project fetched successfully',
+        data: mockResult,
+      });
+    });
+  });
+});
